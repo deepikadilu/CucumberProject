fb288c4dd6402623bfe388fd48871c0c28702726
Deepika Dileep <deepikadilu@gmail.com>
2020-10-18 14:00:24 +0530

    commit new

---
+++ SwiggyMavenProject/src/test/java/runnerTestFiles/SwiggyTestRunner.java
@@ -0,0 +1,9 @@
+package runnerTestFiles;
+
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//
+//@CucumberOptions(tags= "@RegressionTest",features="src\\test\\resources\\Features")
+//public class SwiggyTestRunner extends AbstractTestNGCucumberTests{
+//
+//}
---
+++ SwiggyMavenProject/src/test/java/stepDefinitions/SwiggyMethods.java
@@ -0,0 +1,57 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SwiggyMethods {
+//	WebDriver driver;
+//	String baseUrl;
+//	@Before
+//	public void initialStart() {
+//		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+//		 driver = new ChromeDriver();
+//		baseUrl = "https://www.swiggy.com";
+//	}
+//	@Given("user is in Swiggy application")
+//	public void user_is_in_swiggy_application() {
+//		System.out.println("User in swiggy page");
+//	   driver.get(baseUrl);
+//	}
+//
+//	@When("user seaches {string} in the page")
+//	public void user_seaches_in_the_page(String string) {
+//		System.out.println("User in swiggy page");
+//		
+//	}
+//
+//	@Then("user navigates to homepage")
+//	public void user_navigates_to_homepage() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@And("user clicks on Login button")
+//	public void user_clicks_on_login_button() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("user enters {string}")
+//	public void user_enters(String string) {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("user should navigate to homepage")
+//	public void user_should_navigate_to_homepage() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+
+}
